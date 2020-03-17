845ce37671aab791546d4d5fbb8da51b44d05b92
CheezItMan <chris@adadevelopersacademy.org>
2020-03-17 11:30:22 -0700

    copied worker

--- worker/index.js
+++ worker/index.js
@@ -4,22 +4,16 @@ const redis = require('redis');
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
-  retry_strategy: () => 1000,
+  retry_strategy: () => 1000
 });
-
 const sub = redisClient.duplicate();
 
-const fibCalculator = (index) => {
+function fib(index) {
   if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
 
-  return fibCalculator(index - 1) + fibCalculator(index - 2);
-};
-
-
-// Takes a message, and the channel and message
-// Tells redis to store in a hash called 'values' they key
-// being the 'message' and the value being the result
-// of fibCalculator on that index.
 sub.on('message', (channel, message) => {
-  redisClient.hset('values', message, fibCalculator(parseInt(message)));
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
+sub.subscribe('insert');
